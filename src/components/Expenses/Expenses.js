1a18bdc688a46c7f5302cd0b7500f911538ee992
Jonathan Perez <jonathanperezmota@gmail.com>
2021-07-28 21:09:38 -0400

    Filter date. Adding a filter to filter by date.

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,24 +1,34 @@
+import React, {useState} from 'react';
 import ExpenseItem from './ExpenseItem';
 import './Expenses.css';
 import Card from '../UI/Card'
+import ExpensesFilter from './ExpensesFilter';
 
 function Expenses(props){
+    const [filteredYear, setFilterYear] = useState('2020');
+    const filterChangeHandler = (selectedYear) => {
+        setFilterYear(selectedYear);
+    };
+
 
     return (
-        <Card className="expenses">
-            <ExpenseItem
-                title={props.items[0].title}
-                amount={props.items[0].amount}
-                date={props.items[0].date} />
-            <ExpenseItem
-                title={props.items[1].title}
-                amount={props.items[1].amount}
-                date={props.items[1].date} />
-            <ExpenseItem
-                title={props.items[2].title}
-                amount={props.items[2].amount}
-                date={props.items[2].date} />
-        </Card>
+        <div>
+            <Card className="expenses">
+                <ExpensesFilter defaultYear={filteredYear} onChangeFilter={filterChangeHandler}/>
+                <ExpenseItem
+                    title={props.items[0].title}
+                    amount={props.items[0].amount}
+                    date={props.items[0].date} />
+                <ExpenseItem
+                    title={props.items[1].title}
+                    amount={props.items[1].amount}
+                    date={props.items[1].date} />
+                <ExpenseItem
+                    title={props.items[2].title}
+                    amount={props.items[2].amount}
+                    date={props.items[2].date} />
+            </Card>
+        </div>
     );
 }
 
