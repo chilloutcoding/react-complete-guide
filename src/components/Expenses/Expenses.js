6bf0a23e754664237d1b70b054f05ff8707c9e2d
Jonathan <jonathanperezmota@gmail.com>
2021-08-01 18:59:28 -0400

    Iterating list. Working with conditional content when no filter year is found. Filtering implementation using map along with filter javascript methods.

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -10,23 +10,28 @@ function Expenses(props){
         setFilterYear(selectedYear);
     };
 
+    const filteredExpenses = props.items.filter(expense => {
+        return expense.date.getFullYear().toString() === filteredYear;
+    });
+
+    let expensesContent = <p>No expenses found.</p>;
+    
+    if (filteredExpenses.length > 0) {
+        expensesContent = filteredExpenses.map((expense) => (
+            <ExpenseItem 
+                key={expense.id}
+                title={expense.title}
+                amount={expense.amount}
+                date={expense.date} 
+            />
+        ));
+    }
 
     return (
         <div>
             <Card className="expenses">
                 <ExpensesFilter defaultYear={filteredYear} onChangeFilter={filterChangeHandler}/>
-                <ExpenseItem
-                    title={props.items[0].title}
-                    amount={props.items[0].amount}
-                    date={props.items[0].date} />
-                <ExpenseItem
-                    title={props.items[1].title}
-                    amount={props.items[1].amount}
-                    date={props.items[1].date} />
-                <ExpenseItem
-                    title={props.items[2].title}
-                    amount={props.items[2].amount}
-                    date={props.items[2].date} />
+                {expensesContent}
             </Card>
         </div>
     );
